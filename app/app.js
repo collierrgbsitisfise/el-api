ca24d3bdf483b3cc8b3773cf338eabd94c1cba07
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-04 22:46:49 +0300

    basic express server with configs according .env

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-var a = 'sdfsdf';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const getConfig = require('./configs');
+
+const app = express();
+
+/* Midalwares */
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  /*eslint-disable */
+  console.warn(`Server works on PORT:${PORT}`);
+  console.log(getConfig(process.env.NODE_ENV));
+});
---
+++ app/configs/index.js
@@ -0,0 +1,20 @@
+ /*eslint-disable */
+const configs = {
+  dev: {
+    mongoConnectionUrl: "mongoconectionurl DEV"  
+  },
+  prod: {
+    mongoConnectionUrl: "mongoconectionurl DEV"  
+  },
+};
+
+module.exports = function getConfig(env) {
+  
+  if (!configs[env]) {
+    console.log('Incorrect env!!!');
+    console.log('by default will be used DEV env.')
+    return configs.dev;
+  }
+
+  return configs[env];
+}
