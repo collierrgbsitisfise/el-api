aa655840a4519f1cc48a600a1e8a0400aaf7834f
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-04 23:08:53 +0300

    implemtn morgan and cors midlewars

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
 
 const getConfig = require('./configs');
 
 const app = express();
 
 /* Midalwares */
+
+// CROS ORIGIN REQUEST ALLOW
+app.use(cors());
+app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+
 app.use('/api/v1/es-link', require('./routes/easy-links-routes'));
 
 const PORT = process.env.PORT || 5000;
